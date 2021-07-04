2037ad8acb943af24d39b0eb872ba92c4886ded6
Chakru <abhishek.syncro@gmail.com>
2021-07-04 14:05:03 +0530

    Pagination added

--- src/Components/PetList/Pet.jsx
+++ src/Components/PetList/Pet.jsx
@@ -5,32 +5,56 @@ import './Pet.css';
 
 const petsPerPage = 10;
 
-const Pet = props => {
+const Pet = ({ pets }) => {
+  const [pageNumber, setPageNumber] = useState(0);
+  const pageCount = Math.ceil(pets.length / petsPerPage);
+
   // No Pets Available
   const NA = (
     <div className="na">
       <p>No Pet found 😥</p>
-      <p>Search Again</p>
+      <p>Please Search Again</p>
     </div>
   );
 
   //Main display of the list
   const getListToDisplay = () => {
-    let displayList = props.pets;
+    const pageVisited = pageNumber * petsPerPage;
+    let displayList = [];
     if (displayList.length === 0) {
       return NA;
     }
-    return displayList.map(pet => {
-      const bornDate = new Date(pet.bornAt);
-      const today = new Date();
-      let differenceInDays = today.getTime() - bornDate.getTime();
-      let Difference_In_Days = differenceInDays / (1000 * 3600 * 24);
-      let months = Math.floor(Difference_In_Days.toLocaleString() / 30);
-      return <PetList key={pet.id} name={pet.name} months={months} />;
-    });
+    return displayList
+      .slice(pageVisited, pageVisited + petsPerPage)
+      .map(pet => {
+        const bornDate = new Date(pet.bornAt);
+        const today = new Date();
+        let differenceInDays = today.getTime() - bornDate.getTime();
+        let Difference_In_Days = differenceInDays / (1000 * 3600 * 24);
+        let months = Math.floor(Difference_In_Days.toLocaleString() / 30);
+        return <PetList key={pet.id} name={pet.name} months={months} />;
+      });
   };
 
-  return <>{getListToDisplay()}</>;
+  const changePageHandler = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  return (
+    <>
+      {getListToDisplay()}
+      <ReactPaginate
+        previousLabel={'Previous'}
+        nextLabel={'Next'}
+        pageCount={pageCount}
+        onPageChange={changePageHandler}
+        containerClassName={'paginationButton'}
+        previousLinkClassName={'previousButton'}
+        nextLinkClassName={'nextButton'}
+        activeClassName={'paginationActive'}
+      />
+    </>
+  );
 };
 
 export default Pet;
